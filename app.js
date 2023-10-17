610102a5618042e9b2e570007f31b4301e957a29
cozyroom12 <chewyummy@mandooos-MacBook-Air.local>
2023-10-17 23:09:44 +0900

    add greetings

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-// write sth on console 
-console.log(5353);
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input")
+const greetings = document.querySelector("#greeting")
 
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+
+    localStorage.setItem(USERNAME_KEY, username);
+    greetings.innerText = `Hello ${username}!`;
+    greetings.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function paintGreetings() {}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername);
+
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    greetings.classList.remove(HIDDEN_CLASSNAME);
+    grettings.innerText = `Hello ${savedUsername}`;
+}
