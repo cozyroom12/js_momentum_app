78505c35058343053af3dbce7bf60cc5a63d04db
cozyroom12 <wjddms6841@gmail.com>
2023-10-21 23:35:27 +0900

    add contents in quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,2 +1,15 @@
-const quote = document.querySelector("h2#quote")
+const quotes = [
+    {quote: "random quotes 1."},
+    {author: "somebody famous enough"}
+]
+
+
+const quote = document.querySelector("#quotes span:first-child")
+const quthor = document.querySelector("#quotes span:second-child")
 const author = document.querySelector("h2#author")
+const math = Math.floor(Math.random() * quotes.length)
+
+const todaysQuote = quotes[math]
+
+quote.innerText = todaysQuote.quote
+author.innerText = todaysQuote.author
